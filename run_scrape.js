209124146d87aeb861b650a5e3a7838e1a96fa2d
majirou <majirou2018@gmail.com>
2019-04-07 07:25:33 +0900

    [change] filename

--- run_scrape.js
+++ run_scrape.js
@@ -1,6 +1,6 @@
 console.log("")
 console.log( "@".repeat(process.stdout.columns) )
-console.log("routine", new Date() );
+console.log("routine scraping @ ", new Date() );
 console.log( "@".repeat(process.stdout.columns) )
 const Routine = require("./application/routine.js")
 const r = new Routine()
