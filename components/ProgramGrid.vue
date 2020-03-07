6664e8cd4fb95ab9ee00c36f59c91339aad73801
majirou <majirou2018@gmail.com>
2020-03-08 04:19:28 +0900

    [add] grid with tabulator

---
+++ components/ProgramGrid.vue
@@ -0,0 +1,117 @@
+<template lang="pug">
+  .container-fluid
+    .tbl-header
+      button.btn.btn-sm.btn-primary(@click="showRegisterModal")
+        font-awesome-icon(icon="plus")
+    .tbl-body
+      div(:id="tableId")
+</template>
+
+<script>
+import { dom } from '@fortawesome/fontawesome-svg-core'
+dom.watch()
+
+const Tabulator = require('tabulator-tables')
+
+export default {
+  name: 'ReviewGrid',
+  components: {
+  },
+  props: {
+    'records': {
+      type: Array,
+      default: () => []
+    }
+  },
+  data () {
+    return {
+      tableId: 'table'
+    }
+  },
+  watch: {
+    records () {
+      this.setTabulatorData(this.records)
+    }
+  },
+  mounted () {
+    console.log(this.records)
+    this.initTabulator()
+  },
+  methods: {
+    setTabulatorData (data) {
+      this.table.setData(data)
+    },
+    initTabulator () {
+      this.table = new Tabulator('#' + this.tableId, {
+        height: this.calcHeight('px'),
+        layout: 'fitDataFill',
+        placeholder: 'No Data Set',
+        columns: this.getColumns()
+        // // ajax
+        // ajaxURL: `${this.$apiUrl}/scenarios`,
+        // ajaxProgressiveLoad: 'scroll',
+        // ajaxProgressiveLoadDelay: 200,
+        // // ajaxFiltering: true,
+        // ajaxParams: null,
+        // paginationSize: 10
+      })
+    },
+    calcHeight (unit = null) {
+      const _getHeightWithMargin = (className) => {
+        const _elem = document.getElementsByClassName(className)
+        let _h = 0
+        if (_elem != null) {
+          Object.keys(_elem).forEach((_v) => {
+            const _style = window.getComputedStyle(_elem[_v])
+            _h += parseInt(_style.height) + parseInt(_style.marginTop) + parseInt(_style.marginBottom)
+          })
+        }
+        return _h
+      }
+      const _wh = window.innerHeight
+      const _tbHeight = _getHeightWithMargin('tbl-header')
+      const _tbBody = _getHeightWithMargin('tbl-body')
+      const _h = _wh - _tbHeight - _tbBody
+      return (unit == null) ? _h : `${_h}${unit}`
+    },
+    getColumns () {
+      const columns = [
+        // { formatter: () => '<font-awesome-icon icon="plus" />',
+        { formatter: () => '<i class="fas fa-edit" ></i>',
+          width: 40,
+          align: 'center',
+          cellClick: (event, cell) => {
+            console.log(event, cell)
+            // const ak = cell._cell.row.data._id
+            // this.$router.push({ path: `/review/${ak}` })
+          }
+        },
+        { title: 'ID', field: '_id' },
+        { title: 'シナリオ名', field: 'name' },
+        { title: 'URL', field: 'url' },
+        { title: '次回予定日', field: 'date' },
+        { title: 'DIR', field: 'dir' }
+      ]
+
+      columns.forEach((v, i) => {
+        if (v.headerFilter == null) {
+          columns[i].headerFilter = 'input'
+        }
+      })
+      return columns
+    },
+    showRegisterModal () {
+      this.$emit('modal')
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.tbl-header{
+  margin: 0.5em 0;
+}
+.tbl-body{
+  margin: 0.5em 0;
+}
+</style>
