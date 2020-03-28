255aae0124d52275221844085979e16ea737c3ee
majirou <majirou2018@gmail.com>
2020-03-28 23:04:26 +0900

    [new] mongodb model

---
+++ server/models/MongoDbModel.js
@@ -0,0 +1,53 @@
+class MongoDbModel {
+  constructor () {
+    console.log('mongo db model constructing...'.cyan);
+
+    const config = require('../config/db.config.json')
+    this.url = this._createUrlByConfig(config)
+    this.mongo = require('mongodb').MongoClient
+    this.client = null
+  }
+  /**
+   * create new url to connect mongodb
+   * config is expected follow
+   * "mongodb":{
+   *   "host": String,
+   *   "port": Number,
+   *   "name": String
+   * }
+   * @param {Object} config
+   */
+  _createUrlByConfig(config){
+    return (
+      config != null &&
+      config.host != null &&
+      config.port != null &&
+      config.port > 0
+    ) ? `mongodb://${config.host}:${config.port}/config.name` : null
+  }
+
+  async _connect(){
+    try {
+      if (this.url == null) throw new Error('connecting to:'.cyan + this.url);
+      this.client = await this.mongo.connect(this.url, { useNewUrlParser: true })
+    } catch (err) {
+      console.log(err.name.red, err.message.yellow);
+    }
+  }
+
+  async addProgram(param) {
+    if (this.client == null) {
+      await this._connect()
+    }
+    const collection = await client.db(this.databaseName).collection(this.collectionName)
+    await collection.insertOne(param)
+      .then(res => {
+        console.log('inserted', res.result)
+        this.id = res.insertedId
+      })
+      .catch(err => console.error(err))
+      .then(() => { if (client) client.close() })
+    return this.id
+  }
+}
+module.exports = MongoDbModel
