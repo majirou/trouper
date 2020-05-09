99983000ca2706ea532a69a469f8a0f446f79cc6
majirou <majirou2018@gmail.com>
2020-05-09 12:22:13 +0900

    [add] validate functions

--- server/controllers/ProgramController.js
+++ server/controllers/ProgramController.js
@@ -29,9 +29,31 @@ class ProgramController {
   }
 
   _validateUrl (url) {
+    return /^(https?)(:\/\/[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+)$/.test(url)
   }
 
   _validateSchedule (schedule) {
+    return (
+      schedule != null &&
+      schedule.name != null &&
+      schedule.date != null &&
+      this._validateDate(schedule.date) &&
+      schedule.interval > 0 &&
+      schedule.interval < 3 &&
+      schedule.notification != null &&
+      Array.isArray(schedule.notification) &&
+      schedule.mail != null
+    )
+  }
+
+  _validateDate(date){
+    const arr = date.split("-")
+    return (
+      (arr.length === 3) &&
+      (arr[0] >= 2020 && arr[0] <= 2099) &&
+      (arr[1] >= 1 && arr[1] <= 12) &&
+      (arr[2] >= 1 && arr[2] <= 31)
+    )
   }
 
   _validateActions (actions) {
